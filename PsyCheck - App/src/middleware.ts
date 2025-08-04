9154cf4a88249b5af3852c6dd2b86ff0ac060b8b
Syed Arif Ali <arif.ali.heriotwatt@gmail.com>
2025-08-04 17:33:31 +0400

    chore: configure Supabase SSR middleware

--- PsyCheck - App/src/middleware.ts
+++ PsyCheck - App/src/middleware.ts
@@ -1,5 +1,5 @@
 
-import { createServerClient } from '@supabase/ssr'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
@@ -17,7 +17,7 @@ export async function middleware(request: NextRequest) {
         get(name: string) {
           return request.cookies.get(name)?.value
         },
-        set(name: string, value: string, options) {
+        set(name: string, value: string, options: CookieOptions) {
           request.cookies.set({
             name,
             value,
@@ -34,7 +34,7 @@ export async function middleware(request: NextRequest) {
             ...options,
           })
         },
-        remove(name: string, options) {
+        remove(name: string, options: CookieOptions) {
           request.cookies.set({
             name,
             value: '',
@@ -59,4 +59,8 @@ export async function middleware(request: NextRequest) {
   await supabase.auth.getSession()
 
   return response
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 }
